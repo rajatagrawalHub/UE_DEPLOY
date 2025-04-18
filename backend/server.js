c4fa02f43be4106873282986c2cbff28211ea093
rajatAgrawalHub <rajat.agrawal2022@vitstudent.ac.in>
2025-04-18 15:29:01 +0530

    Deploy Changes

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,8 @@ app.use(
 );
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({origin: '*'}));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
